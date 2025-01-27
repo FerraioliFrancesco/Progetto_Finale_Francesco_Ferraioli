15f887976017289e10995f77741efdf22d280d92
Francesco Ferraioli <francesco-ferraioli@hotmail.it>
2025-01-27 15:54:03 +0100

    navbar inizializzata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <div class="container-fluid">
-        <div class="row justify-content-center min-vh-100">
+        <div class="row justify-content-center text-center min-vh-100">
             <div class="col-12">
                 <h1 class="display-4">
-                    Presto
+                    Presto.it
                 </h1>
             </div>
         </div>
