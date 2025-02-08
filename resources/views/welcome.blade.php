72174c6a9ff736285341af8d598e84085f97c0a9
Francesco Ferraioli <francesco-ferraioli@hotmail.it>
2025-02-08 11:58:52 +0100

    Pronto demo finale

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                     </div>
                     <div class="card-body mt-3">
                         <h5 class="card-title">{{ $article->title }}</h5>
-                        <p class="card-text">{{ $article->price }}</p>
+                        <p class="card-text">{{ $article->price }} €</p>
                         <p class="card-text">{{__("ui.".$article->category->name)  }}</p>
                         <a href="{{ route('article.show', compact('article')) }}"
                             class="button-card py-2 px-3 rounded-pill" data-mdb-ripple-init>Info</a>
